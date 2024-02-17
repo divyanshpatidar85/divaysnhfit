8d2bfe4b164f4d186d7b4da3ce2cd977d19c6e8c
khushmadodiya <divyanshpatidar@gmail.com>
2024-02-17 19:39:23 +0530

    new chages made by dipu

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 
-import 'package:Ashtang/home.dart';
-import 'package:Ashtang/login.dart';
+
+import 'package:fit_bit/home.dart';
+import 'package:fit_bit/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 import 'firebase_options.dart';
 import 'splashScreen.dart';
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,13 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fit_bit/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+// import 'package:kriyeta/main.dart';
+
+// import 'package:fit_bit/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
